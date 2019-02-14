bb92df05f983373b8ff95a68f4fda53e612eef42
Houston <houston.searcy@gatherhere.com>
2019-02-14 10:05:37 -0500

    prettier even

--- packages/sls-test-A/handler.js
+++ packages/sls-test-A/handler.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'This is serverless test A! This is A!',
-      input: event,
-    }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "This is serverless test A!",
+            input: event
+        })
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
